ae0ed058c8d12123f632cb1b746102f2d5e0fcd3
timb eater <110617297+timb-eater@users.noreply.github.com>
2026-04-01 23:42:26 -0400

    finished todos in main class

--- src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
+++ src/main/java/com/mycompany/csc325_oop_designreview_lab/MainClass.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.csc325_oop_designreview_lab;
 
+import java.util.Scanner;
+
 /**
  *
  * @author MoaathAlrajab
@@ -11,34 +13,27 @@ package com.mycompany.csc325_oop_designreview_lab;
 public class MainClass {
 
  public static void main(String[] args) {
-		// ToDo 5: Fix the error
+	Student std1= new Student("James", (short) 20);
+	Freshman std2= new Freshman("James", (short) 20, 12); // name, age, credits
+	Senior std3 = new Senior("John", (short) 30, 90);
 
-		// ToDo 6: Fix the constructor of the Student class
+	Scanner scnr = new Scanner(System.in);
 
-                // Todo 7: Create two classes for Freshman and Senior 
+	 System.out.print("Enter GPA of James (student): ");
+	 double gpa1 = scnr.nextDouble();
+	 std1.setGpa(gpa1);
+	 System.out.print("Enter GPA of James (freshman): "); // theres a difference between student james and freshman james
+	 double gpa2 = scnr.nextDouble();
+	 std2.setGpa(gpa2);
+	 System.out.print("Enter GPA of John (senior): ");
+	 double gpa3 = scnr.nextDouble();
+	 std3.setGpa(gpa3);
 
-                // ToDo 8: The senior class should have a minimum of 85 credits  
+	 System.out.println(std1);
+	 System.out.println(std2);
+	 System.out.println(std3); // the minimum gpa for a senior is 85, any value lower is impossible
 
-		// ToDo 9: Add a toString method for the Student class
-		// ToDo 10: Add a toString method for the Freshman class
-
-		Student std1= new Student("James", (short) 20);
-                // ToDo 11: Add a toString method for the Senior class
-
-		Freshman std1= new Student("James", (short) 20, 12); // name, age, credits
-
-		Senior std2 = new Student("John", (short) 30, 90);
-
-		// ToDo 12: Set the gpa of the student using the scanner and user
-		// 			input and then print the output.
-
-		System.out.println(std1);
-
-                System.out.println(std2);
-
-		// ToDo 13: add comments and explain your code
 
 	}
-
 }
 
